61695e15fd40bd58a4622b01c1156809eba9d12e
walle2022 <103517268+walle2022@users.noreply.github.com>
2022-04-26 11:23:54 -0400

    Create startup.sh

---
+++ Software/startup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#Startup script that will run everytime WALL-E is connected to power and will run automatically without needing a keyboard or hdmi input
+sudo modprobe spidev
+cd Documents
+./edgeDetection.py

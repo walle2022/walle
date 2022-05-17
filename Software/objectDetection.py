d824960f60ff03b373d0d487ae8997827aa3a76d
walle2022 <103517268+walle2022@users.noreply.github.com>
2022-05-17 11:42:12 -0400

    Update objectDetection.py

--- Software/objectDetection.py
+++ Software/objectDetection.py
@@ -4,19 +4,20 @@ import board
 import busio
 import time
 import adafruit_vl53l0x
-import motors
+import adafruit_tca9548a
 
-
-#def detect():
-    # Initialize I2C bus and sensor.
+# Create I2C bus as normal
 i2c = busio.I2C(board.SCL, board.SDA)
-vl53 = adafruit_vl53l0x.VL53L0X(i2c)
-motors.settingUp()
-    # Main loop will read the range and print it every second.
+
+# Create the TCA9548A object and give it the I2C bus
+tca = adafruit_tca9548a.TCA9548A(i2c, address= 0x71)
+
+# For each sensor, create it using the TCA9548A channel instead of the I2C object
+vl531 = adafruit_vl53l0x.VL53L0X(tca[1])
+
 while True:
-    if(vl53.range < 150):
-        motors.stopMotors()
-    else:
-        motors.forwards()
-    print("Range: {0}mm".format(vl53.range))
-   # time.sleep(0.1)
+    print("Range 1: {0}mm".format(vl531.range))
+    if(vl531.range <= 200):
+        print('Object Detected!')
+    time.sleep(0.1)
+
